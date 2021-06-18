b88c218aa5cc71a4d0091fed2e99aa7d71bd347c
louiemc <clouie2@gmail.com>
2021-06-17 20:32:01 -0400

    logged the btnsShowModal var to the console -- the NodeList -not an array. used a for loop to iterate through the NodeList - able to see the text content of the show modal btns

--- script.js
+++ script.js
@@ -7,4 +7,10 @@ const overlay = document.querySelector(".overlay");
 // const btnsShowModal = document.querySelector(".show-modal"); // whenever using a querySelector with a selector with multiple elements, only the first one will get selected
 const btnsShowModal = document.querySelectorAll(".show-modal"); // this gives us a NodeList
 
+// you'll see the NodeList - not an array! (can loop through it)
 console.log(btnsShowModal);
+
+// loop through the NodeList and doo something with the show modal btns
+for (let i = 0; i < btnsShowModal.length; i++) {
+  console.log(btnsShowModal[i].textContent);
+}
