87cbf1fab33cb2b485ebb01356cbec03cae7066a
louiemc <clouie2@gmail.com>
2021-06-17 20:25:43 -0400

    declare variables of elements from HTML, use the right method for the modal buttons since there are multiple elements--the show modal btns

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+// Select the elements we need and store the selections into variables - obv better than selecting the same elements over and over again
+// need to manipulate the modal, close and show, overlay
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+// const btnsShowModal = document.querySelector(".show-modal"); // whenever using a querySelector with a selector with multiple elements, only the first one will get selected
+const btnsShowModal = document.querySelectorAll(".show-modal"); // this gives us a NodeList
+
+console.log(btnsShowModal);
